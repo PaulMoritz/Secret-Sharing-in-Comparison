31d8e7064816ae2003cedf79d5a6dcfc69bb6abe
Paul Ranly <paulmoritz.ranly@stud.tu-darmstadt.de>
2018-05-29 00:31:41 +0200

    calculation of shares now works correctly (with derivatives)

--- share.py
+++ share.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from sympy import *
 import pandas as pd
 import csv
 from math import pow
@@ -16,30 +17,36 @@ datapath = os.path.join(cwd, "DATA")
 
 # creates shares for all Shareholders in one setup
 # needs a message (Integer), a setup and an optional prime number for the finite field
-def share(message, setup, prime=31):
+def share(message, setup, prime_number=31):
     filepath = os.path.join(datapath, setup, 'info.csv')
-    shareholder_list = []
     data = pd.read_csv(filepath, delimiter=',', )
     # create list of number of people in each level
     num = list(data.iloc[4:, 0])
-    for level, number in enumerate(num):
-        for i in range(1, int(number) + 1):
-            shareholder_list.append("s_{}_{}".format(i, level))
+    # create list of thresholds
+    thresholds = list(data.iloc[4:, 1])
+    highest_threshold = max(thresholds)
+    if not highest_threshold == thresholds[-1]:
+        print("Wrong setup for conjunctive structure: threshold for"
+              "level i must always be bigger than threshold(level i-1).")
+        return
+    degree_of_function = int(highest_threshold) - 1
+    # calc number of levels
     # get the number of all shareholders
-    number_of_shareholders = shareholder_list.__len__()
-    # generate random coefficients for 0 < c < prime
-    coefficients = generate_function(number_of_shareholders, message, prime)
+    # generate random coefficients for 0 < c <= prime
+    coefficients = generate_function(degree_of_function, message, prime_number)
     share_list = {}
-    # TODO
-    # ?!? change range for other x values
-    x_values = list(range(1, number_of_shareholders + 1))
-    # calculate the secret shares for each person
-    for i, shareholder in enumerate(shareholder_list):
-        x = x_values[i]
-        value = calc_function(coefficients, x, prime)
-        if value == -1:
-            return
-        share_list[shareholder] = value
+    # create a dict of shareholder:value pairs
+    for level, number in enumerate(num):
+        for person in range(1, int(number) + 1):
+            shareholder = ("s_{}_{}".format(person, level))
+            if person == 1:
+                new_level = true
+            else:
+                new_level = false
+            result = calc_function(coefficients, level, person, new_level, prime_number)
+            share_list[shareholder] = result
+    print(share_list)
+
     # write Shares to 'shares.csv' and save it in the setups directory
     with open(os.path.join(datapath, setup, "shares.csv"), "w", newline='', encoding="utf8") as shares:
         writer = csv.writer(shares)
@@ -49,28 +56,44 @@ def share(message, setup, prime=31):
 
 
 # generates coefficients for the function
-# Format: [a_0, a_1, a_2,...] for f(x) = a_0 + a_1 *x + a_2 *x^2....
+# Format: [[a_0, 0], [a_1, 1, [a_2, 2] ...] for f(x) = a_0 *x^0 + a_1 *x^1 + a_2 *x^2....
 # with a_0 = message
-def generate_function(length, message, prime):
-    coefficients = [message]
-    for i in range(1, length):
-        a_i = np.random.randint(1, prime)
-        coefficients.append(a_i)
+def generate_function(in_degree, message, prime_number):
+    coefficients = [[message, 0]]
+    for i in range(1, in_degree + 1):
+        a_i = np.random.randint(1, prime_number)
+        coefficients.append([a_i, i])
     return coefficients
 
 
-# calculate the y- values for each shareholder with their given x
-def calc_function(coeff_list, x, prime):
-    result = 0
-    for i, coefficient in enumerate(coeff_list):
-        result = (result + (coefficient * pow(x, i)) % prime) % prime
-    if (result % prime) == 0:
-        print("""Error in calculation: Share with Value 0 detected!
-Please try again, consider changing your prime numbers.""")
-        return -1
-    else:
-        return int((result % prime))
+# derivates the given function in place (modulo the prime)
+# for each pair of coefficients te normal derivation rules apply
+# (factor = factor * exponent,
+#  exponent = exponent -1)
+def derivate_function(function_to_derivate, prime_number):
+    for level in function_to_derivate:
+        # TODO Modulo here?
+        level[0] = (level[0] * level[1]) % prime_number
+        # catch x^0, don't subtract exponent here
+        if level[1] > 0:
+            level[1] -= 1
+        else:
+            pass
+    return function_to_derivate
 
-# share_main(3,["s11", "s21", "s22"])
-# share(3,"example")
-# get_x_values(["s11", "s21", "s22"])
+
+# calculate the y- values for each shareholder with their given x
+# new_level is set when the result for the first person from a level is calculated
+# ASSUMING we only calculate the results in ascending order of Level/Person
+def calc_function(coeff_list, level, x, new_level, prime_number):
+    if level > 0 and new_level:
+        return calc_function(derivate_function(coeff_list, prime_number), level - 1, x, true, prime_number)
+    else:
+        result = 0
+        for coefficient in coeff_list:
+            result = (result + (coefficient[0] * pow(x, coefficient[1])) % prime_number) % prime_number
+        return int((result % prime_number))
+
+
+share(3, "example")
+# derivate_function([[3, 0], [27, 1], [16, 2], [18, 3], [7, 4]], 31)
