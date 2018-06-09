c4866a4893336a22d24cdd741bf1a291aed66476
Paul Ranly <paulmoritz.ranly@stud.tu-darmstadt.de>
2018-06-09 18:14:30 +0200

    Finished requirements and threshold checks

--- reconstruct.py
+++ reconstruct.py
@@ -1,6 +1,7 @@
 import os
 from tools import *
 import pandas as pd
+import random
 
 # get path to DATA directory
 cwd = os.getcwd()
@@ -9,37 +10,82 @@ datapath = os.path.join(cwd, "DATA")
 
 # reconstruct([["s1_2", 4], ["s3_2", 3]])
 # alternative formats: "s1,2" ; "1,2" ; "1_2"
-def reconstruct(setup, share_list):
+def reconstruct(setup, number_of_people):
+    # read the list and extract shareholder information
     shares = []
-    coordinates = []
-    filepath = os.path.join(datapath, setup, 'shares.csv')
-    data = pd.read_csv(filepath, skiprows=1, header=None, delimiter=',', )
+    person_ID = []
+    functions_involved = []
+    try:
+        filepath = os.path.join(datapath, setup, 'shares.csv')
+        data = pd.read_csv(filepath, skiprows=0, header=None, delimiter=',', )
+        field_size = data[1][0]
+    except FileNotFoundError as e:
+        print("Shares for setup '{}' not yet created.".format(setup))
+        return
+    # read data of shareholders into tuples
+    tuples = [tuple(x) for x in data.values[2:]]
+    print("All given shareholders: {}".format(tuples))
+    # select a random sample of given shareholders
+    share_list = random.sample(tuples, number_of_people)
+    print("Subset of {} shareholders randomly selected is {}.".format(number_of_people, share_list))
+    # TODO compare input to shares from given setup?!
     for i, shareholder in enumerate(share_list):
-        if not len(shareholder) is 2:
-            print("Invalid input: " + str(len(shareholder)) + " arguments given in shareholder-list [" + str(i) +"]")
-            return
+        name = shareholder[0].split('_')
+        name = name[1:]
+        functions_involved.append(name[1])
         shares.append(shareholder[1])
-        # TODO more cases?
-        if '_' in shareholder[0]:
-            if 's_' in shareholder[0] or 'S_' in shareholder[0]:
-                shareholder[0] = shareholder[0][2:]
-            name = shareholder[0].split('_')
-        elif ',' in shareholder[0]:
-            name = shareholder[0].split(',')
-        else:
-            print("Can't read format for shareholder {}, you need a separator like '_'/','; eg. S1_0".format(i))
-            return
-        name[0] = name[0].strip('s')
-        name[0] = name[0].strip('S')
 
-        coordinates.append((name[0], name[1]))
-    print(coordinates)
-    matrix, highest_derivative = interpolation_matrix(coordinates)
+        person_ID.append((name[0], name[1]))
+    print("Coordinates are {}".format(person_ID))
+
+    phi = sorted(set(functions_involved))
+    print("Share value column for interpolation is {}".format(shares))
+    print("Vector phi of function x^i with i printed is {}".format(phi))
+    matrix, max_person_number, highest_derivative = interpolation_matrix(person_ID)
+    print("\nChecking thresholds:")
+    if not thresholds_fulfilled(setup, person_ID):
+        return
     if requirement_1(matrix, highest_derivative, len(share_list)):
         print("Requirement 1 'Unique Solution' is satisfied.")
     else:
         print("Requirement 1 'Unique Solution' not satisfied with given subset.")
         return
+    if supported_sequence(matrix):
+        print("Requirement 1 'No supported 1-sequence of odd length' is satisfied.")
+    else:
+        print("Requirement 1 'No supported 1-sequence of odd length' not satisfied with given subset.")
+        return
+    if requirement_2(highest_derivative, field_size, max_person_number):
+        print("Requirement 2 'Unique solution over finite field of size {}' is satisfied.".format(field_size))
+    else:
+        print("Requirement 2 'Unique solution over finite field of size {}'"
+              "not satisfied with given subset.".format(field_size))
+        return
+    print("\nAll requirements for a unique solution are given, starting interpolation...")
 
 
-reconstruct("another_example", [["3_1", 4], ["S_2_1", 3], ["s1,0", 5], ["4,1", 4]])
+#
+def thresholds_fulfilled(setup, person_ID):
+    filepath = os.path.join(datapath, setup, 'level_stats.csv')
+    thresholds = []
+    count_of_persons = 0
+    try:
+        data = pd.read_csv(filepath, skiprows=1, header=None, delimiter=',')
+        thresholds = data[1].values
+    except FileNotFoundError as e:
+        print(e)
+    for number_of_level, item in enumerate(thresholds):
+        for person in person_ID:
+            if int(person[1]) == number_of_level:
+                count_of_persons += 1
+        if count_of_persons < int(item):
+            print("Threshold {} not fulfilled, the subset contains only {} people up to this level."
+                  "(Should be at least {})".format(number_of_level, count_of_persons, item))
+            return False
+        else:
+            print("Threshold {} fulfilled.".format(number_of_level))
+    return True
+
+
+reconstruct("another_example1", 4)
+#thresholds_fulfilled("another_example1", [])
--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 # derivate the given function in place (modulo the prime)
@@ -50,8 +51,8 @@ def interpolation_matrix(coordinates):
     interpolation_mat = np.zeros((max_i, max_j + 1))
     for coordinate in coordinates:
         interpolation_mat[int(coordinate[0])-1][int(coordinate[1])] = 1
-    print(interpolation_mat)
-    return interpolation_mat, max_j
+    print("The interpolation matrix is \n {}".format(interpolation_mat))
+    return interpolation_mat,max_i, max_j
 
 
 # TODO correct interpretation of req?
@@ -60,7 +61,7 @@ def requirement_1(matrix, highest_derivative, number_of_points):
     sum_over_matrix = 0
     if not len(matrix) >= number_of_points:
         # level number is > people in subset; --> problem?
-        print("Requirement for size of interpolation matrix not fulfilled. Did you use a shareholder more than once?")
+        print("Requirement for size of interpolation matrix not fulfilled.")
         return
     for k in range(highest_derivative + 1):
         for j in range(k + 1):
@@ -68,5 +69,54 @@ def requirement_1(matrix, highest_derivative, number_of_points):
                 sum_over_matrix += matrix[i-1][j]
         if not sum_over_matrix >= k + 1:
             return False
+    return True
+
+
+# check for a supported 1-sequence of odd length/ second part of the first requirement as stated in Theorem 3
+def supported_sequence(matrix):
+    for j, row in enumerate(matrix):
+        row_sequence_head = 0
+        in_one_sequence = False
+        max_sequence = 0
+        number_of_ones = 0
+        head = 0
+        for i, element in enumerate(row):
+            if element == 1:
+                number_of_ones += 1
+                if not in_one_sequence:
+                    in_one_sequence = True
+                    head = i
+                if i == len(row) - 1:
+                    if number_of_ones > max_sequence:
+                        row_sequence_head = head
+                        max_sequence = number_of_ones
+            else:
+                if in_one_sequence:
+                    in_one_sequence = False
+                    if number_of_ones > max_sequence:
+                        row_sequence_head = head
+                        max_sequence = number_of_ones
+                number_of_ones = 0
+        if max_sequence % 2:
+            if check_supported(matrix, j, row_sequence_head):
+                return True
+    return False
+
+
+def check_supported(matrix, j, i):
+    for row_num, row in enumerate(matrix):
+        if not row_num == j:
+            for index, element in enumerate(row):
+                if index < i:
+                    if element == 1:
+                        return True
+    return False
+
+
+# Requirement 2 from the Appendix (Theorem 4)
+def requirement_2(d, q, max_pers_num):
+    res = 2**(- d + 2) * (d - 1)**((d - 1)/2) * math.factorial(d - 1) * max_pers_num**(((d - 1)*(d - 2))/2)
+    if not float(q) > res:
+        return False
     return True
 
